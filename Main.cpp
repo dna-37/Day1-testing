43fbb2346c088f0b37aa8f439ef239729dda4be2
dna-37 <dannamonzoni@icloud.com>
2025-10-07 00:14:08 +0000

    hello

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+
 int main(){
-  return 1
-    }
+
+
+  int var1, var2, answer;
+
+
+
+  std::cout << "what is the first number: ";
+  std::cin >> var1;
+
+
+ // std::cout << "the first number = " << var1 << "\n";
+
+  std::cout << "what is the second number: ";
+  std::cin >> var2;
+
+  answer = var1 + var2;
+
+  std::cout << var1 << "+" << var2 << "=" <<  answer << "\n";
+
+
+  return 1;
+
+
+
+
+
+}
